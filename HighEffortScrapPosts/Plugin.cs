427490675afb648480c80921126478143b6bd91b
Xilophor <ryanzgreg@gmail.com>
2024-01-17 19:42:12 -0500

    Add GH Workflow Stuff

--- HighEffortScrapPosts/Plugin.cs
+++ HighEffortScrapPosts/Plugin.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 namespace HighEffortScrapPosts;
 
-[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
     private void Awake()
